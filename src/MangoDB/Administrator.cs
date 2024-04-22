69c8dfad9941d82ff0f2b1a98d7a6c549851a0d5
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-04-22 13:39:26 +0100

    🌟 add admin homescreen basics

--- src/MangoDB/Administrator.cs
+++ src/MangoDB/Administrator.cs
@@ -1,7 +1,34 @@
 namespace MangoDB;
 
-    public class Administrator
+public class Administrator
+{
+    public static void HomePage()
     {
-        public static void HomePage() { }
-    }
+        ScrollingMenu homeMenu =
+            new("Please choose an action:", choices: ["Create a new user", "Log out & Exit"]);
+        Window.AddElement(homeMenu);
 
+        while (true)
+        {
+            Window.ActivateElement(homeMenu);
+            var homeMenuResponse = homeMenu.GetResponse();
+            switch (homeMenuResponse!.Value)
+            {
+                case 0:
+                    // TODO: Add CreateNewUser method
+                    break;
+                case 1:
+                    Dialog logoutDialog =
+                        new(["Are you sure you want to log out and exit?"], "No", "Yes");
+                    Window.AddElement(logoutDialog);
+                    Window.ActivateElement(logoutDialog);
+                    var logoutResponse = logoutDialog.GetResponse();
+                    if (logoutResponse!.Value == DialogOption.Right)
+                    {
+                        return;
+                    }
+                    break;
+            }
+        }
+    }
+}
