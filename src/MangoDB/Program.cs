be79e40dda8d68d9bf89937ac0ea91f3662d14c0
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-04-22 09:17:22 +0100

    📦 add package

--- src/MangoDB/Program.cs
+++ src/MangoDB/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
